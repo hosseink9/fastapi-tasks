4aba6cfc565e5558965f1dbfdce77880485cf614
hosseink9 <hosseinkalantari.29@gmail.com>
2025-05-16 22:58:24 +0330

    Fix crud

--- app/crud.py
+++ app/crud.py
@@ -1,27 +1,28 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from . import models, schemas
+from app.models import Task
+from app.schemas import CreateTask
 
 
 async def get_tasks(db: AsyncSession):
-    result = await db.execute(select(models.Task))
+    result = await db.execute(select(Task))
     return result.scalars().all()
 
 
 async def get_task(db: AsyncSession, task_id: int):
-    result = await db.execute(select(models.Task).where(models.Task.id == task_id))
+    result = await db.execute(select(Task).where(Task.id == task_id))
     return result.scalar_one_or_none()
 
 
-async def create_task(db: AsyncSession, task: schemas.CreateTask):
-    db_task = models.Task(**task.model_dump())
+async def create_task(db: AsyncSession, task: CreateTask):
+    db_task = Task(**task.model_dump())
     db.add(db_task)
     await db.commit()
     await db.refresh(db_task)
     return db_task
 
 
-async def update_task(db: AsyncSession, task_id: int, task: schemas.CreateTask):
+async def update_task(db: AsyncSession, task_id: int, task: CreateTask):
     db_task = await get_task(db, task_id)
     if not db_task:
         return None
