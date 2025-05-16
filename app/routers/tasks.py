f7144d4cc379d7108adff1c73d6977b3cc71410e
hosseink9 <hosseinkalantari.29@gmail.com>
2025-05-16 22:58:38 +0330

    Fix code

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 
-from app import schemas, crud
+from app.schemas import ResponseTask, CreateTask
+from app.crud import get_tasks, get_task, create_task, update_task, delete_task
 from app.database import get_db
 
 
@@ -12,35 +13,35 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[schemas.ResponseTask])
-async def read_tasks(db: AsyncSession = Depends(get_db)):
-    return await crud.get_tasks(db)
+@router.get("/", response_model=List[ResponseTask])
+async def _read(db: AsyncSession = Depends(get_db)):
+    return await get_tasks(db)
 
 
-@router.get("/{task_id}", response_model=schemas.ResponseTask)
-async def read_task(task_id: int, db: AsyncSession = Depends(get_db)):
-    task = await crud.get_task(db, task_id)
+@router.get("/{task_id}", response_model=ResponseTask)
+async def _read(task_id: int, db: AsyncSession = Depends(get_db)):
+    task = await get_task(db, task_id)
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
 
 
-@router.post("/", response_model=schemas.ResponseTask)
-async def create_task(task: schemas.CreateTask, db: AsyncSession = Depends(get_db)):
-    return await crud.create_task(db, task)
+@router.post("/", response_model=ResponseTask)
+async def _create(task: CreateTask, db: AsyncSession = Depends(get_db)):
+    return await create_task(db, task)
 
 
-@router.put("/{task_id}", response_model=schemas.ResponseTask)
-async def update_task(task_id: int, task: schemas.CreateTask, db: AsyncSession = Depends(get_db)):
-    updated_task = await crud.update_task(db, task_id, task)
+@router.put("/{task_id}", response_model=ResponseTask)
+async def _update(task_id: int, task: CreateTask, db: AsyncSession = Depends(get_db)):
+    updated_task = await update_task(db, task_id, task)
     if not updated_task:
         raise HTTPException(status_code=404, detail="Task not found")
     return updated_task
 
 
 @router.delete("/{task_id}")
-async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
-    deleted = await crud.delete_task(db, task_id)
+async def _delete(task_id: int, db: AsyncSession = Depends(get_db)):
+    deleted = await delete_task(db, task_id)
     if not deleted:
         raise HTTPException(status_code=404, detail="Task not found")
     return {"ok": True}
